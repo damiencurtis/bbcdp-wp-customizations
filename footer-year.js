01d1438a46f36e4870b432918733e4573d6f9f80
Damien <damiencurtis@live.ie>
2025-08-31 19:06:00 +0100

    Create footer-year.js
    automatically updates the WP footer copyright year annually

---
+++ footer-year.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var yearSpan = document.getElementById('year');
+    if(yearSpan) yearSpan.textContent = new Date().getFullYear();
+});
